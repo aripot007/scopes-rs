cfec65904960c2d3163fc66d3730756978d9782b
Aristide Urli <aristide.urli@telecomnancy.net>
2026-01-03 15:54:37 -0500

    test(macros): Add test for inclusion with include list

--- scopes-macros/src/hierarchy/tests.rs
+++ scopes-macros/src/hierarchy/tests.rs
@@ -36,12 +36,20 @@ fn test_inclusion_transitive() {
     let scope_foo = new_scope("Foo", &["foo"]);
     let scope_foo_bar = new_scope("FooBar", &["foo", "bar"]);
     let scope_foo_bar_baz = new_scope("FooBarBaz", &["foo", "bar", "baz"]);
+    let mut scope_baz = new_scope("Baz", &["baz"]);
+    scope_baz.include.push(scope_foo_bar.ident.clone());
 
-    let g = InclusionGraph::from_scopes([&scope_foo, &scope_foo_bar, &scope_foo_bar_baz].into_iter());
+    let g = InclusionGraph::from_scopes([&scope_foo, &scope_foo_bar, &scope_foo_bar_baz, &scope_baz].into_iter());
 
+    // Check label inclusion
     assert!(is_included(&g, &scope_foo_bar, &scope_foo));
     assert!(is_included(&g, &scope_foo_bar_baz, &scope_foo_bar));
-    assert!(is_included(&g, &scope_foo_bar_baz, &scope_foo))
+    assert!(is_included(&g, &scope_foo_bar_baz, &scope_foo));
+
+    // Check inclusion with include list
+    assert!(is_included(&g, &scope_foo_bar, &scope_baz));
+    assert!(is_included(&g, &scope_foo_bar_baz, &scope_baz));
+    assert!(!is_included(&g, &scope_foo, &scope_baz));
 }
 
 #[test]
