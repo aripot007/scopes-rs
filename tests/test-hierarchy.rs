26fcd2d2e81a2f9b79305d618e764899a746b670
Aristide Urli <aristide.urli@telecomnancy.net>
2026-01-03 18:56:06 -0500

    test: Add test_reference_hierarchy

--- tests/test-hierarchy.rs
+++ tests/test-hierarchy.rs
@@ -2,8 +2,9 @@
 
 use scopes_macros::Scope;
 use scopes_rs::hierarchy::Hierarchized;
+use strum::{EnumIter, IntoEnumIterator};
 
-#[derive(Clone, Debug, PartialEq, Scope)]
+#[derive(Clone, Debug, PartialEq, Scope, EnumIter)]
 enum MyScope {
 
     Foo,
@@ -46,5 +47,48 @@ fn test_label_include() {
 
     assert!(MyScope::Bar.includes(&MyScope::BarReadonly));
     assert!(!MyScope::Bar.includes(&MyScope::FooBar));
+}
+
+// Check that the generated hierarchy corresponds to the expected one
+#[test]
+fn test_reference_hierarchy() {
+
+    fn expected_includes(sself: &MyScope, other: &MyScope) -> bool {
+        if sself == other { return true }
+        match sself {
+            MyScope::Foo => matches!(
+                other, 
+                MyScope::FooBar | MyScope::FooReadonly | MyScope::FooBarReadonly
+            ),
+            MyScope::FooReadonly => matches!(
+                other,
+                MyScope::FooBarReadonly
+            ),
+            MyScope::FooBar => matches!(
+                other,
+                MyScope::FooBarReadonly
+            ),
+            MyScope::FooBarReadonly => false,  // No inclusion
+            MyScope::Readonly => matches!(
+                other,
+                MyScope::BarReadonly | MyScope::FooReadonly  // direct inclusion
+                | MyScope::FooBarReadonly  // indirect inclusion in FooReadonly 
+            ),
+            MyScope::Bar => matches!(
+                other,
+                MyScope::BarReadonly,
+            ),
+            MyScope::BarReadonly => false,  // No inclusion
+        }
+    }
+
+    // Test all combinations
+    for sself in MyScope::iter() {
+        for other in MyScope::iter() {
+
+            assert_eq!(sself.includes(&other), expected_includes(&sself, &other));
+
+        }
+    }
 }
 
